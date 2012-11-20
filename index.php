2b2131e24443fd23e0795572046caf3c5cc082f1
bala <bala@ubuntu.ubuntu-domain>
2012-11-21 02:21:12 +0530

    - First commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>Star Restaurant</title>
+</head>
+<body>
+<form action="menu.php" method="post">
+<center><h1>Star Restaurant</h1></center> 
+<table align="center"><tr><td>Enter username</td><td><input type="text" name="uname"></input></td></tr>
+<tr><td>Enter Email id</td><td><input type="text" name="email"></input></td></tr>
+<tr><td>Enter Phone Number</td><td><input type="text" name="phone"></input></td></tr>
+<tr><td>Enter Password</td><td><input type="password" name="password"></input></td></tr>
+<tr><td colspan=2 align="center"><input type="submit" name="singup"></input></td></tr></table>
+</form>
+</body>
+</html>
